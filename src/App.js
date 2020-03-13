5f81ccd04a0b66adf71ebffb884f82776c828b0d
Mark Kop <mk.marcelo.mk@gmail.com>
2020-03-13 20:47:18 -0300

    feat: change text on click

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  const squares = Array.from({length: 9}, (item, index) =>
-    <button className="square" key={index}>{index}</button>)
+  const [text, setText] = useState('--')
+
+  const squares = Array.from({ length: 9 }, (item, index) => (
+    <button className="square" key={index} onClick={() => setText('X')}>
+      {text}
+    </button>
+  ))
   return (
     <div id="App">
-      <div className="square-container">
-        {squares}
-      </div>
+      <div className="square-container">{squares}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
 describe('App', () => {
   test('has 9 squares', () => {
-    render(<App />);
-    const squares =  document.querySelectorAll('.square')
-    expect(squares).toHaveLength(9);
-  });
+    render(<App />)
+    const squares = document.querySelectorAll('.square')
+    expect(squares).toHaveLength(9)
+  })
+
+  test('has a square that changes text on click', () => {
+    render(<App />)
+    const square = document.querySelector('.square')
+    const oldText = square.innerHTML
+    square.click()
+    const newText = square.innerHTML
+    expect(newText).not.toBe(oldText)
+  })
 })
